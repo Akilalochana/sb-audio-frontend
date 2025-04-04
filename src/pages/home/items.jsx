901dec787e651dc2f7ea56cba1049b4250d9ff3b
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-04-04 19:50:22 +0530

    fixed error in product controller

--- src/pages/home/items.jsx
+++ src/pages/home/items.jsx
@@ -1,4 +1,15 @@
+import axios from "axios"
+import { useEffect } from "react"
+
 export default function Items(){
+    useEffect(()=>{
+       axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`).then((res)=>{
+           console.log(res.data);
+       }).catch((err)=>{
+           console.log(err);
+       })
+    },[])
+
     return(
         <h1>Items</h1>
     )
