ee5dba62524e0a187cbb5606e26712d16d8037e0
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-04-11 00:41:41 +0530

    add multiple images

--- src/pages/admin/addItemPage.jsx
+++ src/pages/admin/addItemPage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import mediaUplod from "../../utils/mediaUplod";
 
 export default function AddItemPage() {
     const [productKey, setProductKey] = useState("");
@@ -10,12 +11,28 @@ export default function AddItemPage() {
     const [productCategory, setProductCategory] = useState("audio");
     const [productDimension, setProductDimension] = useState("");
     const [productDescription, setProductDescription] = useState("");
+    const [productImages, setProductImages] = useState([]);
+
 
     const navigate = useNavigate();
 
 
   
     async function handleAddItems(){
+        // multiple promisses hendle
+        const promises = [];
+
+		//image 4
+		for (let i = 0; i < productImages.length; i++) {
+			console.log(productImages[i]);
+			const promise = mediaUpload(productImages[i]);
+			promises.push(promise);
+			// if(i ==5){
+			// 	toast.error("You can only upload 25 images at a time");
+			// 	break;
+			// }
+		}
+
         console.log(productKey, productName, productPrice, productCategory, productDimension, productDescription);
 
         const token = localStorage.getItem("token");
@@ -23,16 +40,25 @@ export default function AddItemPage() {
 
             try{
 
+                // Promise.all(promises).then((results)=>{
+                //     console.log(results);
+                // }).catch((err) => {
+                //     toast.error(err);
+                // })
 
+                
+                // use async await
+                const imageUrls = await Promise.all(promises);
   
+                //product save in backend
             const result =await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/products/`,{
                 key : productKey,
                 name : productName,
                 price : productPrice,
                 category : productCategory,
                 dimensions : productDimension,
-                description : productDescription
-
+                description : productDescription,
+                images : imageUrls
             },{
                 headers : {
                     Authorization : "Bearer " + token
@@ -101,6 +127,8 @@ export default function AddItemPage() {
                     type="text" 
                     placeholder="Product Description" 
                 />
+
+                <input type="file" onChange={(e) => setProductImages(e.target.files)} multiple className="p-2 border-2 rounded" />
 
                 <button 
                     className="p-2 bg-blue-500 text-white rounded w-full hover:bg-blue-600"
